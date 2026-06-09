792511cefe1c7500907908390d4e0d41cb22a897
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2026-06-09 14:08:16 +0100

    New changes

--- src/component/todo/todo-modal.comp.js
+++ src/component/todo/todo-modal.comp.js
@@ -1,16 +1,30 @@
+import React, { useState } from 'react';
 
-const TodoModalComp = () => {
+const TodoModalComp = ({ closeModal, addTodoItem }) => {
+
+    const [input, setInput] = useState("");
+
+    const createTodo = () => {
+        addTodoItem({
+            title: input,
+            done: false
+        });
+        setInput("");
+        closeModal();
+    }
 
     return (
         <div className="todo_modal">
-            <p className="close">close</p>
+            <p className="close" onClick={ closeModal }>close</p>
 
             <div className="todo_modal_content">
                 <h1>Create Todo</h1>
 
                 <div>
-                    <input type="text" placeholder="Title" />
-                    <button>Create Todo</button>
+                    <input type="text" placeholder="Title" value={input} onChange={(e) => setInput(e.target.value)} />
+                    <button
+                        onClick={ createTodo }
+                    >Create Todo</button>
                 </div>
             </div>
         </div>
--- src/component/todo/todolist.js
+++ src/component/todo/todolist.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 
 import "../../styles.css";
 
@@ -6,29 +7,34 @@ import TodoModalComp from "./todo-modal.comp";
 
 const TodoList = () => {
 
+    const [showModal, setShowModal] = useState(false);
+    const [todoItems, setTodoItems] = useState([]);
+
+    const toggleModal = () => setShowModal(!showModal);
+
+    const addTodoItem = (item) => {
+        setTodoItems([...todoItems, item]);
+    }
+
     return (
         <div>
             <div className="flex_display flex_spacing">
                 <h1>Todo List</h1>
-                <button className="todo_create">Create Todo</button>
+                <button className="todo_create" onClick={ toggleModal }>Create Todo</button>
             </div>
 
             <div>
-                <div className="list_item">
-                    <div className="list_item_box"></div>
-                    <p>Todo 1</p>
-                </div>
-                <div className="list_item">
-                    <div className="list_item_box"></div>
-                    <p>Todo 2</p>
-                </div>
-                <div className="list_item">
-                    <div className="list_item_box"></div>
-                    <p>Todo 3</p>
-                </div>
+                {
+                    todoItems.map((item, index) => (
+                        <div key={index} className="list_item">
+                            <div className="list_item_box"></div>
+                            <p>{ item.title }</p>
+                        </div>
+                    ))
+                }
             </div>
 
-            <TodoModalComp />
+            {showModal && <TodoModalComp closeModal={ toggleModal } addTodoItem={ addTodoItem } />}
         </div>
     )
 }
