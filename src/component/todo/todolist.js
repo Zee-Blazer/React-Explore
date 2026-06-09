303953488871206bf788a9f0eae5c85120bd01d6
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2026-06-09 11:36:39 +0100

    Added some changes for Victor to clone

--- src/component/todo/todolist.js
+++ src/component/todo/todolist.js
@@ -1,20 +1,22 @@
 
+import "../../styles.css";
+
 const TodoList = () => {
 
     return (
         <div>
-            Todo List
+            <h1>Todo List</h1>
 
             <div>
-                <div>
+                <div className="list_item">
                     <div></div>
                     <p>Todo 1</p>
                 </div>
-                <div>
+                <div className="list_item">
                     <div></div>
                     <p>Todo 2</p>
                 </div>
-                <div>
+                <div className="list_item">
                     <div></div>
                     <p>Todo 3</p>
                 </div>
