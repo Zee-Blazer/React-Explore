280f7a4855e689411e829d0a23ee3e2f89eca8b8
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2026-06-09 12:05:25 +0100

    Added new changes to the todolist

---
+++ src/component/todo/todo-modal.comp.js
@@ -0,0 +1,16 @@
+
+const TodoModalComp = () => {
+
+    return (
+        <div>
+            <h1>Create Todo</h1>
+
+            <div>
+                <input type="text" placeholder="Title" />
+                <button>Create Todo</button>
+            </div>
+        </div>
+    )
+}
+
+export default TodoModalComp;
--- src/component/todo/todolist.js
+++ src/component/todo/todolist.js
@@ -5,19 +5,22 @@ const TodoList = () => {
 
     return (
         <div>
-            <h1>Todo List</h1>
+            <div className="flex_display flex_spacing">
+                <h1>Todo List</h1>
+                <button className="todo_create">Create Todo</button>
+            </div>
 
             <div>
                 <div className="list_item">
-                    <div></div>
+                    <div className="list_item_box"></div>
                     <p>Todo 1</p>
                 </div>
                 <div className="list_item">
-                    <div></div>
+                    <div className="list_item_box"></div>
                     <p>Todo 2</p>
                 </div>
                 <div className="list_item">
-                    <div></div>
+                    <div className="list_item_box"></div>
                     <p>Todo 3</p>
                 </div>
             </div>
